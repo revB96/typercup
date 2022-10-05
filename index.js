a7770d7965fb060a56ce88fe569c77ce7affc6aa
Querly96 <showpolska@gmail.com>
2022-10-05 21:06:10 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const express = require("express");
+
+import * as express from 'express';
 const app = express();
 const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");

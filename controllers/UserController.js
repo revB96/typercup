b984ca4bbee701be854fe830310c9832d4692f3e
Querly96 <showpolska@gmail.com>
2022-10-05 20:33:02 +0200

    modified:   controllers/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,7 +8,7 @@ const UserStats = require("./UserStatsController");
 const {getFirstRoundMatch} = require("./ScheduleController")
 const moment = require("moment-timezone");
 const nodemailer = require("nodemailer");
-import "dateFormat";
+import dateFormat from "dateformat";
 const Ticket = require("../models/tickets.js");
 const Round = require("../models/rounds.js");
 

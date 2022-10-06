b8e76c01d36a60d2ffad8c1bd4a53d8fda4ab4ce
Querly96 <showpolska@gmail.com>
2022-10-06 22:21:22 +0200

    modified:   controllers/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -848,6 +848,7 @@ function roundEmailNotification(firstMatch) {
             console.log("Random code: "+ randomCode);
               var nameCapitalized =
                 user.username.charAt(0).toUpperCase() + user.username.slice(1);
+              var userRandomCode = randomCode.code;
 
               const dateOptions = {
                 year: "numeric",
@@ -1334,7 +1335,7 @@ function roundEmailNotification(firstMatch) {
                                                           </td>
                                                           </tr>
                                                       </table>
-                                                      <p>Jeżeli nie możesz wysłać swoich typów, kliknij w ten link aby dodać losowe typy: <a href="https://typer-cup.pl/randomCode/${randomCode.code}" class="f-fallback button" target="_blank">KLIK</a></p>
+                                                      <p>Jeżeli nie możesz wysłać swoich typów, kliknij w ten link aby dodać losowe typy: <a href="https://typer-cup.pl/randomCode/${userRandomCode}" class="f-fallback button" target="_blank">KLIK</a></p>
                                                       
                                               <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
                                               <tr>

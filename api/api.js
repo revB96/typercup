97e47ff73506ffdb19f0a996375983c86a7fc806
Querly96 <showpolska@gmail.com>
2022-10-06 21:50:41 +0200

    modified:   api/api.js

--- api/api.js
+++ api/api.js
@@ -191,7 +191,13 @@ router.post('/admin/user/add', function (req, res) {
 
 router.post('/admin/test/sendRoundNotification', function (req, res) {
     //console.log(req.body)
-    User.testRoundEmailNotification();
+    User.testRoundEmailNotification()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        });
 })
 
 router.post('/user/changepassword', function (req, res) {

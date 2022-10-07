736b6ce951eb746b6d6051b826a4d8860355cbbe
querly <showpolska@gmail.com>
2022-10-07 08:37:43 +0200

    modified:   controllers/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -841,14 +841,11 @@ function roundEmailNotification(firstMatch) {
       //console.log(userNotifications);
       setTimeout(() => {
         userNotifications.forEach((userNotification, index) => {
-          console.log("1")
           getUserById(userNotification.user).then((user) => {
-            //console.log(user._id)
             getUserRandomCode(user._id).then((randomCode) => {
             //console.log("Random code: "+ randomCode);
-              var nameCapitalized =
-                user.username.charAt(0).toUpperCase() + user.username.slice(1);
-              var userRandomCode = randomCode.code;
+              var nameCapitalized = user.username.charAt(0).toUpperCase() + user.username.slice(1);
+              //var userRandomCode = randomCode.code;
 
               const dateOptions = {
                 year: "numeric",
@@ -1335,7 +1332,7 @@ function roundEmailNotification(firstMatch) {
                                                           </td>
                                                           </tr>
                                                       </table>
-                                                      <p>Jeżeli nie możesz wysłać swoich typów, kliknij w ten link aby dodać losowe typy: <a href="https://typer-cup.pl/randomCode/${userRandomCode}" class="f-fallback button" target="_blank">KLIK</a></p>
+                                                      <p>Jeżeli nie możesz wysłać swoich typów, kliknij w ten link aby dodać losowe typy: <a href="https://typer-cup.pl/randomCode/${randomCode.code}" class="f-fallback button" target="_blank">KLIK</a></p>
                                                       
                                               <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
                                               <tr>

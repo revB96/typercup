24659a2a791e8cd91d8c3b92b3f6d08a5edbdff3
Querly96 <showpolska@gmail.com>
2022-10-12 22:12:07 +0200

    modified:   public/js/admin.js 	modified:   views/partials/footer.handlebars

--- public/js/admin.js
+++ public/js/admin.js
@@ -378,7 +378,7 @@ async function adminGetAllRandomCodes() {
   getRandomCodes().then(async (randomCodes) => {
     for await(const [index, randomCode] of Object.entries(randomCodes)) {
       async function loadTable(){
-        $("#pills-randomCodes").append(`
+        await $("#pills-randomCodes").append(`
           <span class="badge badge-dark">${randomCode._id}</span>
           <table class="table table-sm table-hover caption-top">
             <thead class="table-light">
@@ -389,28 +389,33 @@ async function adminGetAllRandomCodes() {
               </tr>
             </thead>
             <tbody>`);
+            return Promise.resolve(1);
       }
       async function loadTableContent(){
-        for (const [index, code] of Object.entries(randomCode.codes)) {
-         $("#pills-randomCodes").append(`
+        for await (const [index, code] of Object.entries(randomCode.codes)) {
+         await $("#pills-randomCodes").append(`
           <tr class="table-danger">
             <th scope="row">${code.round}</th>
             <td>${code.code}</td>
             <td>${code.active}</td>
           </tr>`);
-      }
+        }
+        return Promise.resolve(1);
       }
       async function loadTableEnd(){
-        $("#pills-randomCodes").append(`
+        await $("#pills-randomCodes").append(`
           </tbody>
           </table>
           <span>test</span>
           `)
+          return Promise.resolve(1);
       }
 
-      await loadTable();
-      await loadTableContent();
-      await loadTableEnd();
+      loadTable().then(res => {
+        loadTableContent().then(res2 =>{
+          loadTableEnd()
+        })
+      });
 
     }
   });

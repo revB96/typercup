44419b20bdb9d5e041ac4b3a16b08e516c70bb70
Querly96 <showpolska@gmail.com>
2022-10-16 11:58:30 +0200

    modified:   controllers/UserStatsController.js

--- controllers/UserStatsController.js
+++ controllers/UserStatsController.js
@@ -28,7 +28,7 @@ function add(userId){
 function getAll() {
   var def = Q.defer();
   UserStats.find()
-    .populate("user", "username", "friendlyName")
+    .populate("user", "username")
     .sort({ points: "desc" })
     .exec(function (err, stats) {
       if (err) def.reject(err);

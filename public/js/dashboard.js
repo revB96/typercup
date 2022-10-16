07a4f2215319aca61059ce436ce92cdef3b8e3b8
Querly96 <showpolska@gmail.com>
2022-10-16 21:06:10 +0200

    modified:   public/js/dashboard.js 	modified:   views/partials/footer.handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -57,7 +57,7 @@ function printRoundWithMatches(round) {
               t2w = stats.t2;
               drawn = stats.drawn;
             
-              console.log(t1w)
+              console.log(t2w)
             for (const [index, userTicket] of Object.entries(userTickets)) {
               if (match._id == userTicket.schedule) {
                 if ((userTicket.t1g != null) & (userTicket.t2g != null)) {

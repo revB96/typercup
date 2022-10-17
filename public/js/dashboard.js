3f42f852f2926e99c89b2ba96cab37b8549b2aa8
Querly96 <showpolska@gmail.com>
2022-10-17 19:30:07 +0200

    modified:   public/js/dashboard.js 	modified:   views/partials/footer.handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -57,29 +57,18 @@ function printRoundWithMatches(round) {
           for (const [index, match] of Object.entries(schedule)) {
             getTicketsStats(match._id).then((stats) => {
               var t1g = "",
-                t2g = "",
-                statsDiv = "",
-                ticketColor = "text-white bg-danger";
+                  t2g = "",
+                  statsDiv = "",
+                  ticketColor = "text-white bg-danger";
 
               if (stats.counter > 3) {
                 statsDiv = `<div class="row" style="padding: 10px; margin-top: 10px;">`;
-                if (stats.t1)
-                  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${
-                    stats.t1
-                  }%" aria-valuenow="${
-                    stats.t1
-                  }" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
-                if (stats.t2)
-                  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${
-                    stats.t2
-                  }%" aria-valuenow="${
-                    stats.t2
-                  }" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`;
-                if (stats.drawn)
-                  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ${stats.drawn}%" aria-valuenow="${stats.drawn}" aria-valuemin="0" aria-valuemax="100">REMIS</div>`;
+                if (stats.t1) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${stats.t1}%" aria-valuenow="${stats.t1}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
+                if (stats.t2) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${stats.t2}%" aria-valuenow="${stats.t2}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`;
+                if (stats.drawn)  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ${stats.drawn}%" aria-valuenow="${stats.drawn}" aria-valuemin="0" aria-valuemax="100">REMIS</div>`;
                 statsDiv += `</div>`;
               } else {
-                statsDiv = `<p class="fw-lighter" style="color: white">(Zbyt mało głosów)</div>`;
+                statsDiv = `<p class="fw-lighter" style="color: white; margin: 0; padding: 0;">(Zbyt mało głosów)</div>`;
               }
 
               for (const [index, userTicket] of Object.entries(userTickets)) {

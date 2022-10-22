16520d833f49243fecd408bda48faec71628ab4a
Querly96 <showpolska@gmail.com>
2022-10-22 21:15:10 +0200

    modified:   public/js/table.js 	modified:   views/partials/footer.handlebars

--- public/js/table.js
+++ public/js/table.js
@@ -15,9 +15,9 @@ function listUserTable(){
             
             if(userStat.user.champion == true)  crown = `<img src="img/crown.svg" style="width: 24px; height: 24px"/>`
 
-            if(isNaN(effectiveness) == false)
+            if((isNaN(userStat.correctScore) && isNaN(userStat.correctTeam) && isNaN(result[0].tickets)) == false)
                 effectiveness = ( (userStat.correctScore + userStat.correctTeam) / result[0].tickets) * 100
-            if(isNaN(effectiveness2) == false)
+            if((isNaN(userStat.points) && isNaN(result[0].tickets)) == false)
                 effectiveness2 = (userStat.points / (result[0].tickets * 3)) * 100
 
             $("#users-stat-table").append(`

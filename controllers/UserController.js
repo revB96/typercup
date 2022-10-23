a0a2d13c9a0b672620275306d5532d2fca8c2183
Querly96 <showpolska@gmail.com>
2022-10-23 10:49:14 +0200

    modified:   controllers/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1471,6 +1471,9 @@ function sendReminder(roundDate) {
                   var nameCapitalized =
                     user.username.charAt(0).toUpperCase() +
                     user.username.slice(1);
+                  if(randomCode.code == undefined)
+                    randomCode.code="brak";
+
                   var html = `
                                     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
                                     <html xmlns="http://www.w3.org/1999/xhtml">

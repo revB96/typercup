b5e48774147b554d123c07b86ec7af7fb13567f3
Querly96 <showpolska@gmail.com>
2022-10-23 20:54:08 +0200

    modified:   public/js/dashboard.js 	modified:   views/partials/footer.handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -66,7 +66,7 @@ function printRoundWithMatches(round) {
                 if (stats.t1) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${stats.t1}%" aria-valuenow="${stats.t1}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
                 if (stats.drawn)  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ${stats.drawn}%" aria-valuenow="${stats.drawn}" aria-valuemin="0" aria-valuemax="100">REMIS</div>`;
                 if (stats.t2) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${stats.t2}%" aria-valuenow="${stats.t2}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`
-                statsDiv += `</div>`;
+                statsDiv += `</div><p>Liczba oddanych typów: ${stats.counter}</p>`;
               } else {
                 statsDiv = `<p class="fw-lighter" style="color: white; margin: 0; padding: 0;">(Zbyt mało głosów)</div>`;
               }
@@ -120,7 +120,6 @@ function printRoundWithMatches(round) {
                           </div>
                       </div>
                       ${statsDiv}
-                      <p>Oddano ${stats.counter} typów</p>
                       <p class="card-text"><small>Grupa ${match.group}</small></p>
                   </div>
               </div> 

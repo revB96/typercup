2831660771bc525ca319e76a8a2a1fe6cebbb825
Querly96 <showpolska@gmail.com>
2022-11-01 19:14:30 +0100

    modified:   api/api.js 	modified:   views/admin.handlebars

--- api/api.js
+++ api/api.js
@@ -590,7 +590,7 @@ router.get('/admin/site/edition/get', function (req, res) {
     
 })
 
-router.get('/admin/site/edition/setActive', function (req, res) {
+router.post('/admin/site/edition/setActive', function (req, res) {
     console.log(req.body)
     Site.setActiveEdition(req.body)
         .then(data => {

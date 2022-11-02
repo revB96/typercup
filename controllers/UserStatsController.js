1d09c3c3d7403e6fe3d7b2d71be2d07f2222923f
querly <showpolska@gmail.com>
2022-11-02 11:38:29 +0100

    modified:   controllers/UserStatsController.js

--- controllers/UserStatsController.js
+++ controllers/UserStatsController.js
@@ -42,14 +42,7 @@ function deactivateUser(userId){
 function activateUser(userId){
   var def = Q.defer();
     UserStats.findOneAndUpdate({user:userId}, {
-      $set: { active: true },
-      points: 0,
-      tickets: 0,
-      correctScore: 0,
-      correctTeam: 0,
-      defeat: 0,
-      correctQuestions: 0,
-      quizPoints: 0,
+      active: true
   },{
     new:true,
     autoIndex: true

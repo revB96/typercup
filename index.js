fb3d7ac9f390de194ce114547410055624889e9e
Querly96 <showpolska@gmail.com>
2022-11-05 17:34:08 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const flash = require('express-flash');
 const methodOverride = require('method-override')
 const moment = require('moment-timezone');
 const api = require("./api/api");
-const User = require("./controllers/UserController");
-const Cron = require("./controllers/CronController");
+const Messenger = require("./controllers/MessengerController");
 //const Backup = require("./controllers/BackupController");
 const routes = require('./routes/routes');
 const DB = config.database;
@@ -74,6 +73,6 @@ app.use(express.static('public'))
 
 /*Run Server*/
 app.listen(8081, function () {
-     
+    Messenger.testMessage();
     console.log("Server is running at 1996 port...");
 })

aacb771c7a9873f05f3930d83f54f6243a59a94e
Querly96 <showpolska@gmail.com>
2022-11-05 17:43:34 +0100

    deleted:    controllers/messengerController.js 	modified:   index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const flash = require('express-flash');
 const methodOverride = require('method-override')
 const moment = require('moment-timezone');
 const api = require("./api/api");
-const Messenger = require("./controllers/messengerController");
 //const Backup = require("./controllers/BackupController");
 const routes = require('./routes/routes');
 const DB = config.database;
@@ -73,6 +72,5 @@ app.use(express.static('public'))
 
 /*Run Server*/
 app.listen(8081, function () {
-    Messenger.testMessage();
     console.log("Server is running at 1996 port...");
 })

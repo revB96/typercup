a5698b2db846c08c6350d2a1d556890ff7d210bc
querly <showpolska@gmail.com>
2022-11-09 10:43:04 +0100

    modified:   controllers/CronController.js

--- controllers/CronController.js
+++ controllers/CronController.js
@@ -1,7 +1,5 @@
 const Backup = require('./BackupController')
-const Round = require("./RoundController")
 const User = require("./UserController")
-const nodemailer = require("nodemailer");
 var CronJobManager = require('cron-job-manager')
 
 
@@ -10,21 +8,19 @@ cm = new CronJobManager()
 cm.add('backupDatabase','30 * * * *', ()=>{   //30 * * * *
     console.log("Backup Database...")
     Backup.dumpMongo2Localfile("");
-}, {start: true, timeZone: "Europe/Warsaw"}) //Database backup every day at 8:00
+}, {start: true, timeZone: "Europe/Warsaw"}) 
 
 cm.add('reminder','* * * * *', ()=>{
-    //console.log("Trig reminder")
+    console.log("Trig reminder")
     User.checkReminder()
 }, {start: true, timeZone: "Europe/Warsaw"})
 
 cm.add('closeRound','0 * * * *', function(){
     User.checkCloseRoundNotification();
-    //console.log("Close Round")
+    console.log("Close Round")
 }, {start: true, timeZone: "Europe/Warsaw"})
 
 
-var jobs = cm.listCrons();
-
-console.log(jobs)
+console.log(`I got the current jobs: ${cm}`)
 
   

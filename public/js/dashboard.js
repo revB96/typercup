a2fbd835890a7daa92490b6ae029628cb72615bc
Querly96 <showpolska@gmail.com>
2022-11-13 10:15:36 +0100

    modified:   public/js/dashboard.js 	modified:   views/partials/footer.handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -67,9 +67,9 @@ function printRoundWithMatches(round) {
 
               if (stats.counter > 3) {
                 statsDiv = `<div class="row" style="padding: 10px; margin-top: 10px;">`;
-                if (stats.t1) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${stats.t1}%" aria-valuenow="${stats.t1}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
+                if (stats.t1) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${stats.t1}%" aria-valuenow="${stats.t1}" aria-valuemin="0" aria-valuemax="100"><span style="height: 8px; width: 8px;" class="flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
                 if (stats.drawn)  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ${stats.drawn}%" aria-valuenow="${stats.drawn}" aria-valuemin="0" aria-valuemax="100">=</div>`;
-                if (stats.t2) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${stats.t2}%" aria-valuenow="${stats.t2}" aria-valuemin="0" aria-valuemax="100"><span class="flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`
+                if (stats.t2) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${stats.t2}%" aria-valuenow="${stats.t2}" aria-valuemin="0" aria-valuemax="100"><span style="height: 8px; width: 8px;" class="flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`
                 statsDiv += `</div><small>Liczba oddanych typów: ${stats.counter}</small>`;
               } else {
                 statsDiv = `<p class="fw-lighter" style="color: white; margin: 0; padding: 0;">(Zbyt mało głosów)</div>`;

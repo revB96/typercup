149f1889f832050e7c75519cf27347e758aed7cc
Querly96 <showpolska@gmail.com>
2022-11-15 20:00:07 +0100

    modified:   public/js/dashboard.js 	modified:   views/partials/footer.handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -66,7 +66,7 @@ function printRoundWithMatches(round) {
                   ticketColor = "text-white bg-danger";
             
               if (stats.counter > 3) {
-                statsDiv = `<div class="row" style="padding: 10px; margin-top: 10px;"><div class="progress" style="background: none; height: 20px;">`;
+                statsDiv = `<div class="row" style="margin-top: 20px;"><div class="progress" style="background: none; height: 20px;">`;
                 if (stats.t1) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: ${stats.t1}%" aria-valuenow="${stats.t1}" aria-valuemin="0" aria-valuemax="100"><span class="mt-1 flag-icon flag-icon-${match.t1.shortcut.toLowerCase()}"></div>`;
                 if (stats.drawn)  statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ${stats.drawn}%" aria-valuenow="${stats.drawn}" aria-valuemin="0" aria-valuemax="100"><img style="width:20px; height: 20px;" src="img/handshake.png" /></div>`;
                 if (stats.t2) statsDiv += `<div class="progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: ${stats.t2}%" aria-valuenow="${stats.t2}" aria-valuemin="0" aria-valuemax="100"><span class="mt-1 flag-icon flag-icon-${match.t2.shortcut.toLowerCase()}"></div>`

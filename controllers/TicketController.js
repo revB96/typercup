91339d23b9c047dcb62d685ebd833fe899d4f6e2
Querly96 <showpolska@gmail.com>
2022-11-21 23:10:51 +0100

    modified:   controllers/TicketController.js

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -5,7 +5,7 @@ const Ticket = require("../models/tickets.js");
 const Round = require("../models/rounds.js");
 const RandomCode = require("../models/randomCodes.js");
 const { updateUserStats } = require("./UserStatsController.js");
-const User = require("../models/Users.js");
+const User = require("../models/users.js");
 const Schedule = require("./ScheduleController.js");
 const moment = require('moment-timezone');
 const nodemailer = require("nodemailer");
@@ -84,7 +84,7 @@ function getRunningRound(){
   return def.promise;
 }
 
-function getUserTimezone(userID) {
+function getUserTimezone2(userID) {
   var def = Q.defer();
   User.findOne({ _id: userID })
     .select("timezone")
@@ -98,7 +98,7 @@ function getUserTimezone(userID) {
 
 function checkIfRoundIsOpen(userID){
   var def = Q.defer();
-  getUserTimezone(userID).then(timezone =>{
+  getUserTimezone2(userID).then(timezone =>{
     
     var userTZ, offsetTZ;
     if(timezone = "UK") {userTZ = "Europe/London"; offsetTZ = 1}

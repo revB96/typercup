fe1484e76a90442a6c08d2d92f32dd8922637bc1
Querly96 <showpolska@gmail.com>
2022-11-22 09:09:51 +0100

    modified:   public/js/dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -52,7 +52,7 @@ function printRoundWithMatches(round) {
               `${roundDate.toLocaleDateString(
                 "pl-PL",
                 dateOptions
-              )}<br /> Godzina zamknięcia kolejki: ${closeTime.getHours() - timeoffset - 1}:${minutes} ${spinner}`
+              )}<br /> Godzina zamknięcia kolejki: ${closeTime.getHours() - 1}:${minutes} ${spinner}`
             );
           else {
             spinner = `<button class="btn btn-sm btn-light" type="button" disabled>
@@ -63,7 +63,7 @@ function printRoundWithMatches(round) {
               `${roundDate.toLocaleDateString(
                 "pl-PL",
                 dateOptions
-              )}<br /> Kolejka została zamknięta o: ${closeTime.getHours() - timeoffset - 1}:${minutes} ${spinner}`
+              )}<br /> Kolejka została zamknięta o: ${closeTime.getHours() - 1}:${minutes} ${spinner}`
             );
 
             $(`#dashboard-message`).html(

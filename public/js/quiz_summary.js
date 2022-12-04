271b95cdd7dee9ae0cc7c7cc1af18569542ce8a7
Querly96 <showpolska@gmail.com>
2022-12-04 22:19:46 +0100

    modified:   public/js/quiz_summary.js 	modified:   views/partials/footer.handlebars

--- public/js/quiz_summary.js
+++ public/js/quiz_summary.js
@@ -1,26 +1,31 @@
+function printQuizQuestions(){
+    getQuestions().then(questions => {
+        var quiz_questions = `<th scope="col">User</th>`
+        questions.forEach((question,index) =>{
+            var correct_answer;
+            if(question.correctAnswer == "yes")
+                correct_answer = `Tak`
+            else if(question.correctAnswer == "no")
+                correct_answer = "Nie"
+            else
+                correct_answer = question.correctAnswer
+
+            if(question.correctAnswer == "")
+                quiz_questions += `<th scope="col">${index+1}<br /><br /><br /><small class="text-muted">${correct_answer}</small></th>`
+            else if(question.correctAnswer.length > 10)
+                quiz_questions += `<th scope="col">${index+1}<br /><small class="text-muted">${correct_answer}</small></th>`
+            else
+                quiz_questions += `<th scope="col">${index+1}<br /><br /><small class="text-muted">${correct_answer}</small></th>`
+        })
+        $("#quiz-summary-questions").append(quiz_questions)
+    })
+}
 function printQuizSummary(){
     getAllUsernames().then(users =>{
-        getQuestions().then(questions => {
-            var quiz_questions = `<th scope="col">User</th>`
-            var quiz_answers="";
-            questions.forEach((question,index) =>{
-                var correct_answer;
-                if(question.correctAnswer == "yes")
-                    correct_answer = `Tak`
-                else if(question.correctAnswer == "no")
-                    correct_answer = "Nie"
-                else
-                    correct_answer = question.correctAnswer
-
-                if(question.correctAnswer == "")
-                    quiz_questions += `<th scope="col">${index+1}<br /><br /><br /><small class="text-muted">${correct_answer}</small></th>`
-                else if(question.correctAnswer.length > 10)
-                    quiz_questions += `<th scope="col">${index+1}<br /><small class="text-muted">${correct_answer}</small></th>`
-                else
-                    quiz_questions += `<th scope="col">${index+1}<br /><br /><small class="text-muted">${correct_answer}</small></th>`
-
         users.forEach(user => {
             getUserAnswers(user._id).then(async answers => {
+                getQuestions().then(questions => {
+                
                 var nickname = user.username;
                 var textSize="";
                 var answers_content=""
@@ -29,6 +34,11 @@ function printQuizSummary(){
                     textSize="font-size:11px;"
 
                 answers.answers.forEach(async (ans,index) =>{
+                    console.log(ans)
+                    var correct_answer = questions.find(result => {
+                        return result._id == ans._id
+                    })
+                    console.log(correct_answer)
                     if(index != 0){
                         if(ans.answer == "yes"){
                             answers_content += `<td>Tak</td>`
@@ -39,29 +49,22 @@ function printQuizSummary(){
                         }
                     }
                 })
-
-                quiz_answers += `
-                <tr>
-                    <th style="${textSize}" scope="row">${user.username}</th>
-                    ${answers_content}
-                </tr>`;
-
+                $("#quiz-summary-questions").append(quiz_questions)
                 $("#quiz-summary-answers").append(`
                 <tr>
                     <th style="${textSize}" scope="row">${user.username}</th>
                     ${answers_content}
-                </tr>`);
+                </tr>
+            `);
             })
+        })
         });
-        console.log(quiz_answers)
-        $("#quiz-summary-questions").html(quiz_questions)
-    })
-})
     })
 }
 
 $(document).ready(function () {
     if (document.title == "Typer Cup | Podsumowanie Quizu") {
+      printQuizQuestions();
       printQuizSummary();
     }
 })

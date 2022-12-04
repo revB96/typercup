5259cd7ebcdd5b1c26b3fa8775802f7d232051e2
Querly96 <showpolska@gmail.com>
2022-12-04 20:12:47 +0100

    modified:   public/js/quiz_summary.js 	modified:   views/partials/footer.handlebars

--- public/js/quiz_summary.js
+++ public/js/quiz_summary.js
@@ -7,26 +7,26 @@ function printQuizSummary(){
                 $("#quiz-summary-answers").append(`
             <tr>
                 <th scope="row">${user.username}</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>${answers.answers[1]}</td>
+                <td>${answers.answers[2]}</td>
+                <td>${answers.answers[3]}</td>
+                <td>${answers.answers[4]}</td>
+                <td>${answers.answers[5]}</td>
+                <td>${answers.answers[6]}</td>
+                <td>${answers.answers[7]}</td>
+                <td>${answers.answers[8]}</td>
+                <td>${answers.answers[9]}</td>
+                <td>${answers.answers[10]}</td>
+                <td>${answers.answers[11]}</td>
+                <td>${answers.answers[12]}</td>
+                <td>${answers.answers[13]}</td>
+                <td>${answers.answers[14]}</td>
+                <td>${answers.answers[15]}</td>
+                <td>${answers.answers[16]}</td>
+                <td>${answers.answers[17]}</td>
+                <td>${answers.answers[18]}</td>
+                <td>${answers.answers[19]}</td>
+                <td>${answers.answers[20]}</td>
             </tr>
             `);
             })
